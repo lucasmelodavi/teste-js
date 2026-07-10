666f46b04ce50eee84b38b55d0d91d04d243de04
Davi Lucas Melo <lucasmelodavi425@gmail.com>
2026-07-10 14:18:52 -0300

    primeiro commit

---
+++ aula2.js/tipo-de-dados.js
@@ -0,0 +1,21 @@
+/* Tipos de Dados em JavaScript 
+    
+    string - texto
+    number - número
+    boolean - verdadeiro(true) ou falso(false)
+    array - lista de valores
+    object - objeto com propriedades e valores
+
+*/
+  
+   const texto = " exemplo de string"; // string
+
+   const numero = 190 / 10 ; // number
+
+   const variavelBooleana = true; // boolean
+
+   const array = [1, 2, 3, 4, 5]; // array
+
+   const objeto = { nome: "João", idade: 30 }; // object
+
+   console.log(numero, texto, variavelBooleana, array, objeto); // imprime 19 " exemplo de string" true [1, 2, 3, 4, 5] { nome: "João", idade: 30 }
---
+++ aula2.js/variavies.js
@@ -0,0 +1,17 @@
+/* Variaveis em JavaScript 
+   
+  const - constante, não pode ser alterada
+  let - variável, pode ser alterada
+  var - variável, pode ser alterada, mas não é recomendada para uso
+
+*/
+
+const banana = "fruta"; // constante
+
+console.log(banana); // imprime "fruta"
+
+let banana2 = "fruta"; // variável
+
+banana2 = "cor"; // variável
+
+console.log(banana2); // imprime "cor"
