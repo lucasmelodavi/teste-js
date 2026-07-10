9804dc314228315bbf4db2360c7f6e591af5493e
Davi Lucas Melo <lucasmelodavi425@gmail.com>
2026-07-10 15:06:52 -0300

    aula 3

---
+++ aula2/tipo-de-dados.js
@@ -0,0 +1,23 @@
+/* Tipos de Dados em JavaScript 
+    
+    string - texto
+    number - número
+    boolean - verdadeiro(true) ou falso(false)
+    array - lista de valores
+    object - objeto com propriedades e valores
+    Null - valor nulo
+    Undefined - valor indefinido
+
+*/
+
+const texto = " exemplo de string"; // string
+
+const numero = 190 / 10; // number
+
+const variavelBooleana = true; // boolean
+
+const usuario1 = [ "João", "Maria", "José" ]; // array
+
+const usuario2 = { nome: "João", idade: 30, solteiro: false, esposa: null }; // object
+
+console.log(numero, texto, variavelBooleana, usuario1[2], usuario2.solteiro); // imprime 19 " exemplo de string" true [1, 2, 3, 4, 5] { nome: "João", idade: 30 }
---
+++ aula3/if-else.js
@@ -0,0 +1,37 @@
+/* O que é if?
+
+O if significa:
+
+"Se..."
+
+Ele serve para verificar se uma condição é verdadeira.
+
+O que é else?
+
+O else significa:
+
+"Senão..."
+
+Ele é usado junto com o if.
+
+Dados de Comparação
+ Operador  Significado    
+
+ >      Maior que      
+ <      Menor que      
+ >=     Maior ou igual
+ <=      Menor ou igual 
+ ===     Igual 
+ ==     Igual (não recomendado)         
+ !==    Diferente      
+
+ n
+*/
+
+const notaDoAluno = 5.9
+
+if(notaDoAluno >= 6) {
+    console.log("Aluno aprovado")
+} else {
+    console.log("Aluno reprovado")
+}
